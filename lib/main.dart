938cbe9d446d9c06a0926b44ea0d75c2c3a80ccb
sabers <momagdy194@gmail.com>
2020-08-04 20:53:43 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/subjects.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// Streams are created so that app can respond to notification-related events since the plugin is initialised in the `main` function
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+    BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String> selectNotificationSubject =
+    BehaviorSubject<String>();
+
+NotificationAppLaunchDetails notificationAppLaunchDetails;
+
+class ReceivedNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String payload;
+
+  ReceivedNotification({
+    @required this.id,
+    @required this.title,
+    @required this.body,
+    @required this.payload,
+  });
+}
+
+/// IMPORTANT: running the following code on its own won't work as there is setup required for each platform head project.
+/// Please download the complete example app from the GitHub repository where all the setup has been done
+Future<void> main() async {
+  // needed if you intend to initialize in the `main` function
+  WidgetsFlutterBinding.ensureInitialized();
+
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+  // Note: permissions aren't requested here just to demonstrate that can be done later using the `requestPermissions()` method
+  // of the `IOSFlutterLocalNotificationsPlugin` class
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+      onDidReceiveLocalNotification:
+          (int id, String title, String body, String payload) async {
+        didReceiveLocalNotificationSubject.add(ReceivedNotification(
+            id: id, title: title, body: body, payload: payload));
+      });
+  var initializationSettings = InitializationSettings(
+      initializationSettingsAndroid, initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    selectNotificationSubject.add(payload);
+  });
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class PaddedRaisedButton extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const PaddedRaisedButton({
+    @required this.buttonText,
+    @required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
+      child: RaisedButton(child: Text(buttonText), onPressed: onPressed),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final MethodChannel platform =
+      MethodChannel('crossingthestreams.io/resourceResolver');
+
+
+
+  @override
+  void dispose() {
+    didReceiveLocalNotificationSubject.close();
+    selectNotificationSubject.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Plugin example app'),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+
+                  PaddedRaisedButton(
+                    buttonText:
+                        'يسطي دا بيعيد كل يوم الساعه 10 الصبح ',
+                    onPressed: () async {
+                      await _showDailyAtTime();
+                    },
+                  ),
+                  PaddedRaisedButton(
+                    buttonText:
+                        'عبدوووو دا بيعيد كل يوم اتنين الساعه 10 بردو يعني كل اسبوع يسطي ',
+                    onPressed: () async {
+                      await _showWeeklyAtDayAndTime();
+                    },
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+  Future<void> _showDailyAtTime() async {
+    var time = Time(10, 0, 0);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'repeatDailyAtTime channel id',
+        'repeatDailyAtTime channel name',
+        'repeatDailyAtTime description');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+        0,
+        'show daily title',
+        'Daily notification shown at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
+        time,
+        platformChannelSpecifics);
+  }
+
+  Future<void> _showWeeklyAtDayAndTime() async {
+    var time = Time(10, 0, 0);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'show weekly channel id',
+        'show weekly channel name',
+        'show weekly description');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(
+        0,
+        'show weekly title',
+        'Weekly notification shown on Monday at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
+        Day.Monday,
+        time,
+        platformChannelSpecifics);
+  }
+
+
+  String _toTwoDigitString(int value) {
+    return value.toString().padLeft(2, '0');
+  }
+}
+
+
---
+++ test_driver/flutter_local_notifications_e2e.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+import 'package:e2e/e2e.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  E2EWidgetsFlutterBinding.ensureInitialized();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  group('initialize()', () {
+    setUpAll(() async {
+      flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    });
+    testWidgets('can initialise', (WidgetTester tester) async {
+      final initializationSettingsAndroid =
+          AndroidInitializationSettings('app_icon');
+      final initializationSettingsIOS = IOSInitializationSettings();
+      final initializationSettings = InitializationSettings(
+          initializationSettingsAndroid, initializationSettingsIOS);
+      final initialised = await flutterLocalNotificationsPlugin
+          .initialize(initializationSettings);
+      expect(initialised, isTrue);
+    });
+  });
+  group('resolvePlatformSpecificImplementation()', () {
+    setUpAll(() async {
+      flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+      final initializationSettingsAndroid =
+          AndroidInitializationSettings('app_icon');
+      final initializationSettingsIOS = IOSInitializationSettings();
+      final initializationSettings = InitializationSettings(
+          initializationSettingsAndroid, initializationSettingsIOS);
+      await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    });
+
+    if (Platform.isIOS) {
+      testWidgets('Can resolve iOS plugin implementation when running on iOS',
+          (WidgetTester tester) async {
+        expect(
+            flutterLocalNotificationsPlugin
+                .resolvePlatformSpecificImplementation<
+                    IOSFlutterLocalNotificationsPlugin>(),
+            isA<IOSFlutterLocalNotificationsPlugin>());
+      });
+    }
+
+    if (Platform.isAndroid) {
+      testWidgets(
+          'Can resolve Android plugin implementation when running on Android',
+          (WidgetTester tester) async {
+        expect(
+            flutterLocalNotificationsPlugin
+                .resolvePlatformSpecificImplementation<
+                    AndroidFlutterLocalNotificationsPlugin>(),
+            isA<AndroidFlutterLocalNotificationsPlugin>());
+      });
+    }
+
+    if (Platform.isIOS) {
+      testWidgets(
+          'Returns null trying to resolve Android plugin implementation when running on iOS',
+          (WidgetTester tester) async {
+        expect(
+            flutterLocalNotificationsPlugin
+                .resolvePlatformSpecificImplementation<
+                    AndroidFlutterLocalNotificationsPlugin>(),
+            isNull);
+      });
+    }
+    if (Platform.isAndroid) {
+      testWidgets(
+          'Returns null trying to resolve iOS plugin implementation when running on Android',
+          (WidgetTester tester) async {
+        expect(
+            flutterLocalNotificationsPlugin
+                .resolvePlatformSpecificImplementation<
+                    IOSFlutterLocalNotificationsPlugin>(),
+            isNull);
+      });
+    }
+
+    testWidgets('Throws argument error requesting base class type',
+        (WidgetTester tester) async {
+      expect(
+          () => flutterLocalNotificationsPlugin
+              .resolvePlatformSpecificImplementation(),
+          throwsArgumentError);
+    });
+  });
+}
